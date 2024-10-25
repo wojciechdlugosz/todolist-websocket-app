a32d679357654498b27568972da0cd14317eebb0
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-25 13:33:13 +0200

    Prepare server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
