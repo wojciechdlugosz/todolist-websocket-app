0e287a7a7cb276f29aa1706afd402ecf15e00e6f
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-27 10:49:19 +0100

    Add basic listeners to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const server = app.listen(process.env.PORT || 8000, () => {
@@ -8,3 +9,21 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
+
+const io = socket(server);
+
+// listen
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
+    socket.on('addTask', (newTask) => {
+        tasks.push(newTask);
+        socket.broadcast.emit('addTask', newTask);
+    });
+    socket.on('removeTask', (id) => {
+        const taskIndex = tasks.findIndex(task => task.id === id);
+        tasks.splice(taskIndex, 1);
+        socket.broadcast.emit('removeTask', id);
+    });
+});
+
+const tasks = [];
